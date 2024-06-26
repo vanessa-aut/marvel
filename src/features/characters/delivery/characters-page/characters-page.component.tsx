87b1ec72c0a6d4872d45606e9b92cfc255ec4d80
vanessa <vanessa.medina@izertis.com>
2024-06-26 13:20:40 +0200

    feature: add debounce for searching

--- src/features/characters/delivery/characters-page/characters-page.component.tsx
+++ src/features/characters/delivery/characters-page/characters-page.component.tsx
@@ -4,11 +4,13 @@ import React, { type FC, useState } from 'react'
 import { SearchBoxComponent } from '../../../../core/delivery/search-box/search-box.component'
 import { CharactersList } from '../list/characters-list.component'
 import { useSearch } from '../../hooks/use-search'
+import useDebounce from '../../hooks/use-debounce'
 
 export const CharactersPageComponent: FC = () => {
   const [searchValue, setSearchValue] = useState('')
-  const { characters, isSearching } = useSearch(searchValue)
-  const onChangeHandle = (event: React.ChangeEvent<HTMLInputElement>) => console.log(event.currentTarget.value)
+  const debouncedSearchValue = useDebounce(searchValue, 500)
+  const { characters, isSearching } = useSearch(debouncedSearchValue)
+  const onChangeHandle = (event: React.ChangeEvent<HTMLInputElement>) => setSearchValue(event.currentTarget.value)
 
   return (
     <>

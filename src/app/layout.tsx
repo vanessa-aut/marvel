aa013178404af56487d05365431039b4742879bd
vanessa <vanessa.medina@izertis.com>
2024-06-27 12:30:48 +0200

    feature: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,22 @@
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import { Roboto, Roboto_Condensed } from 'next/font/google'
 import './globals.scss'
 import { ReactNode } from 'react'
 import { FavoriteCharactersProvider } from '../features/characters/providers/favorite-characters.provider'
 import { HeaderComponent } from '../core/delivery/header/header.component'
 
-const roboto = Roboto({ weight: '100', subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ['100', '400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  variable: '--font-roboto',
+})
+const roboto_condensed = Roboto_Condensed({
+  weight: ['100', '400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  variable: '--font-roboto-condensed',
+})
 
 export const metadata: Metadata = {
   title: 'Marvel App',
@@ -18,8 +29,8 @@ export default function RootLayout({
   children: ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>
+    <html lang="en" className={`${roboto.variable} ${roboto_condensed.variable}`}>
+      <body>
         <FavoriteCharactersProvider>
           <HeaderComponent />
           <main>

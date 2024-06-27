6de3774c7e7ca3f1c6cb5651f516a253b55ebf2c
vanessa <vanessa.medina@izertis.com>
2024-06-27 12:31:57 +0200

    feature: add favorites page

---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,5 @@
+import { FavoritesPageComponent } from '../../features/characters/delivery/favorites-page/favorites-page.component'
+
+export default async function Favorites() {
+  return <FavoritesPageComponent />
+}
---
+++ src/features/characters/delivery/favorites-page/favorites-page.component.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React, { type FC, useState } from 'react'
+import { SearchBoxComponent } from '../../../../core/delivery/search-box/search-box.component'
+import { CharactersList } from '../list/characters-list.component'
+import useDebounce from '../../hooks/use-debounce'
+import { useCharacterSearch } from '../../hooks/use-character-search'
+import styles from './favorites-page.module.scss'
+
+export const FavoritesPageComponent: FC = () => {
+  const [searchValue, setSearchValue] = useState('')
+  const debouncedSearchValue = useDebounce(searchValue, 500)
+  const { characters, isSearching } = useCharacterSearch(debouncedSearchValue, true)
+  const onChangeHandle = (event: React.ChangeEvent<HTMLInputElement>) => setSearchValue(event.currentTarget.value)
+
+  return (
+    <>
+      <h1 className={styles.title}>Favorites</h1>
+      {isSearching ? (
+        <h2>Loading</h2>
+      ) : (
+        <>
+          <SearchBoxComponent value={searchValue} onChangeHandle={onChangeHandle} total={characters.total} />
+          <CharactersList characters={characters.results} />
+        </>
+      )}
+    </>
+  )
+}

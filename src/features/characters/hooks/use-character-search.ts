a3c3af895c90b10fcbac0da499da202c71ec83eb
vanessa <vanessa.medina@izertis.com>
2024-06-27 11:52:13 +0200

    feature: hook includes favorites logic

--- src/features/characters/hooks/use-character-search.ts
+++ src/features/characters/hooks/use-character-search.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import { Character } from '../domain/character'
 import { ApiResponse } from '../../../core/http/api-response'
+import { useFavoriteCharacters } from './use-favorite-characters'
 
-export const useCharacterSearch = (searchParam: string) => {
+export const useCharacterSearch = (searchParam: string, isFavs: boolean = false) => {
   const [characters, setCharacters] = useState<ApiResponse<Character>>({ total: 0, results: [] })
   const [isSearching, setIsSearching] = useState(true)
+  const { favorites } = useFavoriteCharacters()
 
   const getCharacters = async () => {
     const params = new URLSearchParams({ nameStartsWith: searchParam || '' })
@@ -12,11 +14,22 @@ export const useCharacterSearch = (searchParam: string) => {
     return await charactersResponse.json()
   }
 
+  const filteredFavorites = () =>
+    favorites.filter(character => character.name.toLowerCase().startsWith(searchParam.toLowerCase()))
+
+  const getFavoriteCharacters = () => {
+    const favChars = searchParam ? filteredFavorites() : favorites
+    return {
+      total: favChars.length,
+      results: favChars,
+    }
+  }
+
   const onSearch = async () => {
     setIsSearching(true)
 
     try {
-      const charactersResponse = await getCharacters()
+      const charactersResponse = isFavs ? getFavoriteCharacters() : await getCharacters()
       setCharacters(charactersResponse)
     } catch {
       throw new Error('Error fetching')
